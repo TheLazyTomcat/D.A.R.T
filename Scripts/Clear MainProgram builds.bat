18ac24f3f9590b6b62303650558f8f428f83131b
František Milt <frantisek.milt@gmail.com>
2015-09-28 01:42:13 +0200

    Added building/clearing scripts

---
+++ Scripts/Clear MainProgram builds.bat
@@ -0,0 +1,7 @@
+﻿@echo off
+
+del ..\MainProgram\Delphi\Release\win_x86\SCS_Unlocker.exe /S /Q  
+del ..\MainProgram\Lazarus\Release\win_x86\SCS_Unlocker.exe /S /Q 
+del ..\MainProgram\Lazarus\Release\win_x64\SCS_Unlocker.exe /S /Q 
+del ..\MainProgram\Lazarus\Debug\win_x86\SCS_Unlocker.exe /S /Q  
+del ..\MainProgram\Lazarus\Debug\win_x64\SCS_Unlocker.exe /S /Q
