74e7c2b520eb208b4858ff9237dc1ee63e490a40
František Milt <frantisek.milt@gmail.com>
2018-03-29 15:40:10 +0200

    Added CRC32 library, added format unit for ZIP archives (types, constants, ...), added processing settings structures

---
+++ Source/DART_Format_ZIP.pas
@@ -0,0 +1,154 @@
+unit DART_Format_ZIP;
+
+{$INCLUDE DART_defs.inc}
+
+interface
+
+uses
+  AuxTypes, CRC32;
+
+{===============================================================================
+--------------------------------------------------------------------------------
+                          ZIP structures and constants
+--------------------------------------------------------------------------------
+===============================================================================}
+
+//--- ZIP archive signature ----------------------------------------------------
+
+const
+  DART_ZIP_FileSignature = UInt32($04034b50);
+
+//--- Bit flags ----------------------------------------------------------------
+
+  DART_ZBF_Encrypted        = UInt16($0001);  // bit 0
+  DART_ZBF_DataDescriptor   = UInt16($0008);  // bit 3
+  DART_ZBF_StrongEncryption = UInt16($0040);  // bit 6
+
+//--- Signatures ---------------------------------------------------------------
+
+  DART_ZIP_LocalFileHeaderSignature            = UInt32($04034b50);
+  DART_ZIP_DataDescriptorSignature             = UInt32($08074b50);
+  DART_ZIP_CentralDirectoryFileHeaderSignature = UInt32($02014b50);
+  DART_ZIP_EndOfCentralDirectorySignature      = UInt32($06054b50);
+
+type
+//--- Local file header --------------------------------------------------------
+
+  TDART_ZIP_LocalFileHeaderRecord = packed record
+    Signature:              UInt32;
+    VersionNeededToExtract: UInt8;
+    OSNeededForExtraction:  UInt8;
+    GeneralPurposeBitFlag:  UInt16;
+    CompressionMethod:      UInt16;
+    LastModFileTime:        UInt16;
+    LastModFileDate:        UInt16;
+    CRC32:                  TCRC32;
+    CompressedSize:         UInt32;
+    UncompressedSize:       UInt32;
+    FileNameLength:         UInt16;
+    ExtraFieldLength:       UInt16;
+  end;
+
+  TDART_ZIP_LocalFileHeader = record
+    BinPart:    TDART_ZIP_LocalFileHeaderRecord;
+    FileName:   AnsiString;
+    ExtraField: AnsiString;
+  end;
+
+//--- Data descriptor record ---------------------------------------------------
+
+  TDART_ZIP_DataDescriptorRecord = packed record
+    Signature:        UInt32;
+    CRC32:            TCRC32;
+    CompressedSize:   UInt32;
+    UncompressedSize: UInt32;
+  end;
+
+//--- Central directory file header --------------------------------------------
+
+  TDART_ZIP_CentralDirectoryFileHeaderRecord = packed record
+    Signature:                    UInt32;
+    VersionMadeBy:                UInt8;
+    HostOS:                       UInt8;
+    VersionNeededToExtract:       UInt8;
+    OSNeededForExtraction:        UInt8;
+    GeneralPurposeBitFlag:        UInt16;
+    CompressionMethod:            UInt16;
+    LastModFileTime:              UInt16;
+    LastModFileDate:              UInt16;
+    CRC32:                        TCRC32;
+    CompressedSize:               UInt32;
+    UncompressedSize:             UInt32;
+    FileNameLength:               UInt16;
+    ExtraFieldLength:             UInt16;
+    FileCommentLength:            UInt16;
+    DiskNumberStart:              UInt16;
+    InternalFileAttributes:       UInt16;
+    ExternalFileAttributes:       UInt32;
+    RelativeOffsetOfLocalHeader:  UInt32;
+  end;
+
+  TDART_ZIP_CentralDirectoryFileHeader = record
+    BinPart:      TDART_ZIP_CentralDirectoryFileHeaderRecord;
+    FileName:     AnsiString;
+    ExtraField:   AnsiString;
+    FileComment:  AnsiString;
+  end;
+
+//--- End of central directory record ------------------------------------------
+
+  TDART_ZIP_EndOfCentralDirectoryRecord = packed record
+    Signature:                        UInt32;
+    NumberOfThisDisk:                 UInt16;
+    CentralDirectoryStartDiskNumber:  UInt16;
+    EntriesOnDisk:                    UInt16;
+    Entries:                          UInt16;
+    CentralDirectorySize:             UInt32;
+    CentralDirectoryOffset:           UInt32;
+    CommentLength:                    UInt16;
+  end;
+
+  TDART_ZIP_EndOfCentralDirectory = record
+    BinPart:  TDART_ZIP_EndOfCentralDirectoryRecord;
+    Comment:  AnsiString;
+  end;
+
+//--- Entry utility data -------------------------------------------------------
+
+  TDART_ZIP_EntryUtilityData = record
+  (*
+    OriginalLocalHeaderOffset:  UInt32;   // stores offset of local header in input file while processing it into output
+    DataOffset:                 Int64;    // offset of actual entry data from the start of archive
+    NeedsCRC32:                 Boolean;  // CRC32 has to be recalculated
+    NeedsSizes:                 Boolean;  // actual sizes needs to be obtained
+    Erroneous:                  Boolean;  // entry is erroneous but the error was ignored
+  *)  
+  end;
+
+//--- Main structure -----------------------------------------------------------
+
+  TDART_ZIP_Entry = record
+    LocalHeader:            TDART_ZIP_LocalFileHeader;
+    DataDescriptor:         TDART_ZIP_DataDescriptorRecord;
+    CentralDirectoryHeader: TDART_ZIP_CentralDirectoryFileHeader;
+    UtilityData:            TDART_ZIP_EntryUtilityData;
+  end;
+
+  TDART_ZIP_Entries = record
+    Arr:    array of TDART_ZIP_Entry;
+    Count:  Integer;
+  end;
+
+  TDART_ZIP_ArchiveStructure = record
+    Entries:                TDART_ZIP_Entries;
+    EndOfCentralDirectory:  TDART_ZIP_EndOfCentralDirectory;
+  end;
+
+//--- Other ZIP constants ------------------------------------------------------
+
+const
+  DART_ZIP_PathDelim = '/';
+
+implementation
+
+end.
---
+++ Source/DART_ProcessingSettings.pas
@@ -0,0 +1,201 @@
+unit DART_ProcessingSettings;
+
+{$INCLUDE DART_defs.inc}
+
+interface
+
+type
+  TDARTArchiveType = (atUnknown,atSCS_sig,atSCS_frc,atZIP_sig,atZIP_frc,atZIP_dft);
+  TDARTRepairMethod = (rmUnknown,rmRebuild,rmExtract,rmConvert);
+
+const
+  DART_ArchiveTypeStrings: array[TDARTArchiveType] of String =
+    ('Unknown','SCS#','SCS# (forced)','ZIP','ZIP (forced)','ZIP (defaulted)');
+
+  DART_RepairMethodStrings: array[TDARTRepairMethod] of String =
+    ('Unknown','Rebuild archive','Extract archive','Convert archive');
+
+//--- Common processing settings -----------------------------------------------
+
+type
+  TDART_PS_Common = record
+    ArchivePath:            String;
+    OriginalArchiveType:    TDARTArchiveType;
+    SelectedArchiveType:    TDARTArchiveType;
+    RepairMethod:           TDARTRepairMethod;
+    TargetPath:             String;
+    IgnoreFileSignature:    Boolean;
+    InMemoryProcessing:     Boolean;
+    IgnoreErroneousEntries: Boolean;
+  end;
+
+  TDART_PS_Auxiliaty = record
+    InMemoryProcessingAllowed:  Boolean;  // depends on available memory and size of input archive
+  end;
+
+//--- ZIP-specific processing settings -----------------------------------------
+
+  TDART_PS_ZIP_EndOfCentralDirectory = record
+    IgnoreEndOfCentralDirectory:    Boolean;
+    IgnoreDiskSplit:                Boolean;
+    IgnoreNumberOfEntries:          Boolean;
+    IgnoreCentralDirectoryOffset:   Boolean;
+    IgnoreComment:                  Boolean;
+    LimitSearch:                    Boolean;
+  end;
+
+  TDART_PS_ZIP_CentralDirectory = record
+    IgnoreCentralDirectory:       Boolean;
+    IgnoreSignature:              Boolean;
+    IgnoreVersions:               Boolean;
+    ClearEncryptionFlags:         Boolean;
+    IgnoreCompressionMethod:      Boolean;
+    IgnoreModTime:                Boolean;
+    IgnoreModDate:                Boolean;
+    IgnoreCRC32:                  Boolean;
+    IgnoreSizes:                  Boolean;
+    IgnoreInternalFileAttributes: Boolean;
+    IgnoreExternalFileAttributes: Boolean;
+    IgnoreLocalHeaderOffset:      Boolean;
+    IgnoreExtraField:             Boolean;
+    IgnoreFileComment:            Boolean;
+  end;
+
+  TDART_PS_ZIP_LocalHeader = record
+    IgnoreLocalHeaders:       Boolean;
+    IgnoreSignature:          Boolean;
+    IgnoreVersions:           Boolean;
+    ClearEncryptionFlags:     Boolean;
+    IgnoreCompressionMethod:  Boolean;
+    IgnoreModTime:            Boolean;
+    IgnoreModDate:            Boolean;
+    IgnoreCRC32:              Boolean;
+    IgnoreSizes:              Boolean;
+    IgnoreFileName:           Boolean;
+    IgnoreExtraField:         Boolean;
+    IgnoreDataDescriptor:     Boolean;
+  end;
+
+  TDART_PS_ZIP = record
+    AssumeCompressionMethods: Boolean;
+    EndOfCentralDirectory:    TDART_PS_ZIP_EndOfCentralDirectory;
+    CentralDirectory:         TDART_PS_ZIP_CentralDirectory;
+    LocalHeader:              TDART_PS_ZIP_LocalHeader;
+  end;
+
+//--- SCS#-specific processing settings ----------------------------------------
+
+  TDART_PS_SCS = record
+  end;
+
+//--- Main structure -----------------------------------------------------------
+
+  TDARTArchiveProcessingSettings = record
+    Common:       TDART_PS_Common;
+    ZIP:          TDART_PS_ZIP;
+    SCS:          TDART_PS_SCS;
+    Auxiliary:    TDART_PS_Auxiliaty;    
+  end;
+
+//==============================================================================
+
+//--- Default archive processing settings --------------------------------------
+
+const
+  DART_DefaultArchiveProcessingSettings: TDARTArchiveProcessingSettings = (
+    Common: (
+      ArchivePath:            '';
+      OriginalArchiveType:    atUnknown;
+      SelectedArchiveType:    atUnknown;
+      RepairMethod:           rmRebuild;
+      TargetPath:             '';
+      IgnoreFileSignature:    True;
+      InMemoryProcessing:     False;
+      IgnoreErroneousEntries: False);
+    ZIP: (
+      AssumeCompressionMethods: False;
+      EndOfCentralDirectory: (
+        IgnoreEndOfCentralDirectory:  False;
+        IgnoreDiskSplit:              True;
+        IgnoreNumberOfEntries:        False;
+        IgnoreCentralDirectoryOffset: False;
+        IgnoreComment:                True;
+        LimitSearch:                  True);
+      CentralDirectory: (
+        IgnoreCentralDirectory:       False;
+        IgnoreSignature:              True;
+        IgnoreVersions:               True;
+        ClearEncryptionFlags:         True;
+        IgnoreCompressionMethod:      True;
+        IgnoreModTime:                False;
+        IgnoreModDate:                False;
+        IgnoreCRC32:                  True;
+        IgnoreSizes:                  False;
+        IgnoreInternalFileAttributes: False;
+        IgnoreExternalFileAttributes: False;
+        IgnoreLocalHeaderOffset:      False;
+        IgnoreExtraField:             True;
+        IgnoreFileComment:            True);
+      LocalHeader: (
+        IgnoreLocalHeaders:           False;
+        IgnoreSignature:              True;
+        IgnoreVersions:               True;
+        ClearEncryptionFlags:         True;
+        IgnoreCompressionMethod:      True;
+        IgnoreModTime:                False;
+        IgnoreModDate:                False;
+        IgnoreCRC32:                  True;
+        IgnoreSizes:                  True;
+        IgnoreFileName:               False;
+        IgnoreExtraField:             True;
+        IgnoreDataDescriptor:         False));
+    Auxiliary: (
+      InMemoryProcessingAllowed: False));
+
+//==============================================================================
+
+//--- Processing settings functions --------------------------------------------
+
+procedure RectifyArchiveProcessingSettings(var APS: TDARTArchiveProcessingSettings);
+
+procedure RectifyZIPProcessingSettings(var ZIP_PS: TDART_PS_ZIP);
+procedure RectifySCSProcessingSettings(var SCS_PS: TDART_PS_SCS);
+
+implementation
+
+procedure RectifyArchiveProcessingSettings(var APS: TDARTArchiveProcessingSettings);
+begin
+If not APS.Auxiliary.InMemoryProcessingAllowed then
+  APS.Common.InMemoryProcessing := False;
+end;
+
+//------------------------------------------------------------------------------
+
+procedure RectifyZIPProcessingSettings(var ZIP_PS: TDART_PS_ZIP);
+begin
+If ZIP_PS.CentralDirectory.IgnoreCentralDirectory or ZIP_PS.CentralDirectory.IgnoreLocalHeaderOffset then
+  ZIP_PS.LocalHeader.IgnoreLocalHeaders := False;
+If ZIP_PS.CentralDirectory.IgnoreCentralDirectory then
+  begin
+    If ZIP_PS.LocalHeader.IgnoreCompressionMethod then
+      ZIP_PS.LocalHeader.IgnoreSizes := False;
+    If ZIP_PS.LocalHeader.IgnoreSizes then
+      ZIP_PS.LocalHeader.IgnoreCompressionMethod := False;
+    ZIP_PS.LocalHeader.IgnoreFileName := False;
+  end
+else
+  begin
+    If ZIP_PS.CentralDirectory.IgnoreCompressionMethod then
+      ZIP_PS.CentralDirectory.IgnoreSizes := False;
+    If ZIP_PS.CentralDirectory.IgnoreSizes then
+      ZIP_PS.CentralDirectory.IgnoreCompressionMethod := False;
+  end;
+end;
+
+//------------------------------------------------------------------------------
+
+procedure RectifySCSProcessingSettings(var SCS_PS: TDART_PS_SCS);
+begin
+end;
+
+end.
